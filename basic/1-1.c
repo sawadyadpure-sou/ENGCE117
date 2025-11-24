d26e2ae4a335b95e68eef8cf8c8e14d29c29c2e1
sawadyadpure-sou <sawadyadpure.pure@gmail.com>
2025-11-24 13:16:16 +0700

    update 1-1

--- basic/1-1.c
+++ basic/1-1.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+int main()
+{
+    int N, i;
 
-int main() {
-    printf("Hello, World!");
-    return 0;
+    scanf("%d" , &N);
+    if (N <= 0)
+    {
+
+        printf("Error");
+
+    }
+    for (i = 1; i <= N; i++)
+    {
+        printf("Hello Loop!\n");
+    }
+
+    
+    
+
 }
