952fa49ea83058f0f5f56e4ef43646304c5e1cfa
sawadyadpure-sou <sawadyadpure.pure@gmail.com>
2025-11-24 13:44:34 +0700

    update 1-2

---
+++ basic/1-2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    int point;
+
+    scanf("%d" , &point);
+    if (point >= 80)
+    {
+        printf("Grade A");
+    }
+    else if (point >= 70)
+    {
+        printf("Grade B");
+    }
+    else if (point >= 60)
+    {
+        printf("Grade C");
+    }
+    else if (point >= 50)
+    {
+        printf("Grade D");
+    }
+    else if (point < 50)
+    {
+        printf("Grade F");
+    }
+    
+    
+    
+    
+
+    
+    
+
+}
