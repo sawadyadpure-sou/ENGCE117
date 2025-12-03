fefb646ae89efb31b23d12088d7640ad03fde6ca
sawadyadpure-sou <sawadyadpure.pure@gmail.com>
2025-12-03 18:53:03 +0700

    update lab1-5

---
+++ basic/lab1-5.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void GetMatrix(int **value, int *row, int *col);
+
+int main() 
+{
+    int *data;
+    int m, n;
+
+    GetMatrix(&data, &m, &n);
+
+    if (data != NULL && m > 0 && n > 0) 
+    {
+        printf("\nMatrix Output:\n");
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) 
+            {
+                printf("%d ", data[i * n + j]);
+            }
+            printf("\n");
+        }
+    } else {
+        printf("\nNo matrix to display.\n");
+    }
+
+    return 0;
+}
+
+void GetMatrix(int **value, int *row, int *col) 
+{
+    printf("Enter rows: ");
+    scanf("%d", row);
+    printf("Enter cols: ");
+    scanf("%d", col);
+
+    *value = (int *)malloc((*row) * (*col) * sizeof(int));
+
+    if (*value == NULL) 
+    {
+        *row = 0;
+        *col = 0;
+        return;
+    }
+
+    printf("Enter %d elements:\n", (*row) * (*col));
+    for (int i = 0; i < *row; i++) 
+    {
+        for (int j = 0; j < *col; j++) 
+        {
+            scanf("%d", &(*value)[i * (*col) + j]);
+        }
+    }
+}
