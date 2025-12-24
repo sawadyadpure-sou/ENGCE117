b164dbd4cabed6e0b1f2cd0d66e2fe451013c55c
sawadyadpure-sou <sawadyadpure.pure@gmail.com>
2025-12-24 18:32:54 +0700

    x

--- basic/lab3/lab3-2.c
+++ basic/lab3/lab3-2.c
@@ -7,29 +7,26 @@ struct student {
     float gpa ;
 } ;
 
-void upgrade( struct student child ) ;
+void upgrade( struct student *child ) ;
 
 int main() {
     struct student aboy ;
-
     aboy.sex = 'M' ;
     aboy.gpa = 3.00 ;
 
     printf("Original GPA: %.2f\n", aboy.gpa) ;
 
-    upgrade( aboy ) ;
+    upgrade( &aboy ) ;
 
     printf("Upgraded GPA: %.2f\n", aboy.gpa) ;
 
     return 0 ;
 }
 
-void upgrade( struct student child ) {
-
-    if ( child.sex == 'M' ) {
-        child.gpa = child.gpa + ( child.gpa * 0.10 ) ;
-    } else if ( child.sex == 'F' ) {
-        child.gpa = child.gpa + ( child.gpa * 0.20 ) ;
+void upgrade( struct student *child ) {
+    if ( child->sex == 'M' ) {
+        child->gpa = child->gpa + (child->gpa * 0.10) ;
+    } else if ( child->sex == 'F' ) {
+        child->gpa = child->gpa + (child->gpa * 0.20) ;
     }
-
 }
