d725939715ff6de7a37837706da2a57e8c3863c7
sawadyadpure-sou <sawadyadpure.pure@gmail.com>
2025-12-24 18:59:13 +0700

    update lab3-4

--- basic/lab3/lab3-4.c
+++ basic/lab3/lab3-4.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
-// สมมติโครงสร้าง struct student (เพื่อให้ code คอมไพล์ผ่าน)
 struct student {
-    char name[50] ;
+    char name[ 50 ] ;
     int age ;
 } ;
 
@@ -20,11 +19,10 @@ void GetStudent( struct student child[][ 10 ], int *room ) {
     scanf( "%d", room ) ;
 
     for ( int i = 0 ; i < *room ; i++ ) {
-        printf( "Room %d:\n", i + 1 ) ;
         for ( int j = 0 ; j < 10 ; j++ ) {
-            printf( "  Student %d name: ", j + 1 ) ;
+            printf( "Room %d, Student %d name: ", i + 1, j + 1 ) ;
             scanf( "%s", child[ i ][ j ].name ) ;
-            printf( "  Student %d age: ", j + 1 ) ;
+            printf( "Room %d, Student %d age: ", i + 1, j + 1 ) ;
             scanf( "%d", &child[ i ][ j ].age ) ;
         }
     }
