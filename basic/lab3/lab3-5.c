a68ae0b5a28fc964afab256b3d92d04344c29917
sawadyadpure-sou <sawadyadpure.pure@gmail.com>
2025-12-24 19:15:24 +0700

    update lab3-5

--- basic/lab3/lab3-5.c
+++ basic/lab3/lab3-5.c
@@ -6,7 +6,6 @@ struct student {
     int age ;
 } ;
 
-// Prototype ตามโจทย์: ฟังก์ชันคืนค่าเป็น Pointer ไปยัง Array ขนาด 10
 struct student ( *GetStudent( int *room ) )[ 10 ] ;
 
 int main() {
@@ -25,14 +24,12 @@ struct student ( *GetStudent( int *room ) )[ 10 ] {
     struct student ( *child )[ 10 ] = malloc( ( *room ) * sizeof( *child ) ) ;
 
     for ( int i = 0 ; i < *room ; i++ ) {
-        printf( "Room %d:\n", i + 1 ) ;
         for ( int j = 0 ; j < 10 ; j++ ) {
-            printf( "  Student %d name: ", j + 1 ) ;
+            printf( "Room %d, Student %d name: ", i + 1, j + 1 ) ;
             scanf( "%s", child[ i ][ j ].name ) ;
-            printf( "  Student %d age: ", j + 1 ) ;
+            printf( "Room %d, Student %d age: ", i + 1, j + 1 ) ;
             scanf( "%d", &child[ i ][ j ].age ) ;
         }
     }
-
-    return child ; 
+    return child ;
 }
