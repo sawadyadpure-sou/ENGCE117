837b612327e9801f41e93e86a8672e1992dc1be2
sawadyadpure-sou <sawadyadpure.pure@gmail.com>
2025-12-24 20:41:56 +0700

    update lab4-6

--- basic/lab4/lab4-6.c
+++ basic/lab4/lab4-6.c
@@ -1,52 +1,55 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h> 
+#include <stdlib.h>
 
 struct studentNode {
-    char name[ 20 ] ;
-    int age ;
-    char sex ;
-    float gpa ;
-    struct studentNode *next ;
-} ;
+    char name[20];
+    int age;
+    char sex;
+    float gpa;
+    struct studentNode *next;
+};
 
-void SaveNode( struct studentNode *child, char n[], int a, char s, float g ) ;
-void GoNext2( struct studentNode ***walk ) ;
+void saveNode(struct studentNode *child, char n[], int a, char s, float g);
+void GoNext2(struct studentNode ***walk);
 
 int main() {
-    struct studentNode *start, *now1, **now2 ;
+    struct studentNode *start, *now1, **now2;
 
-    start = ( struct studentNode* )malloc( sizeof( struct studentNode ) ) ;
-    SaveNode( start, "one", 6, 'M', 3.11 ) ;
+    start = (struct studentNode*)malloc(sizeof(struct studentNode));
+    saveNode(start, "one", 6, 'M', 3.11);
 
-    start->next = ( struct studentNode* )malloc( sizeof( struct studentNode ) ) ;
-    SaveNode( start->next, "two", 8, 'F', 3.22 ) ;
+    start->next = (struct studentNode*)malloc(sizeof(struct studentNode));
+    saveNode(start->next, "two", 8, 'F', 3.22);
 
-    start->next->next = ( struct studentNode* )malloc( sizeof( struct studentNode ) ) ;
-    SaveNode( start->next->next, "three", 10, 'M', 3.33 ) ;
+    start->next->next = (struct studentNode*)malloc(sizeof(struct studentNode));
+    saveNode(start->next->next, "three", 10, 'M', 3.33);
 
-    start->next->next->next = ( struct studentNode* )malloc( sizeof( struct studentNode ) ) ;
-    SaveNode( start->next->next->next, "four", 12, 'F', 3.44 ) ;
+    start->next->next->next = (struct studentNode*)malloc(sizeof(struct studentNode));
+    saveNode(start->next->next->next, "four", 12, 'F', 3.44);
+    start->next->next->next->next = NULL; 
 
-    now1 = start ;
-    now2 = &start ; 
+    now1 = start;
+    now2 = &start;
 
-    GoNext2( &now2 ) ;
-    printf( "Now - 1 %s ", ( *now2 )->name ) ;
+    GoNext2(&now2);
 
-    return 0 ;
+    printf("%s\n", (*now2)->name);
+
+    return 0;
 }
 
-void SaveNode( struct studentNode *child, char n[], int a, char s, float g ) {
-    strcpy( child->name, n ) ;
-    child->age = a ;
-    child->sex = s ;
-    child->gpa = g ;
-    child->next = NULL ; 
+void saveNode(struct studentNode *child, char n[], int a, char s, float g) {
+    strcpy(child->name, n);
+    child->age = a;
+    child->sex = s;
+    child->gpa = g;
+    child->next = NULL;
 }
 
-void GoNext2( struct studentNode ***walk ) {
-    if ( *walk != NULL && ( **walk ) != NULL && ( **walk )->next != NULL ) {
-        **walk = ( **walk )->next ;
+void GoNext2(struct studentNode ***walk) {
+
+    if (*walk != NULL && (**walk)->next != NULL) {
+        *walk = &((**walk)->next);
     }
 }
