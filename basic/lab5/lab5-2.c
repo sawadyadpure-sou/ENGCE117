bc0688149a6fa73cd709a1ba83b7f05b48dbfb6d
pure <sawadyad.pure@gmail.com>
2026-01-09 11:36:38 +0700

    update lab5-2

---
+++ basic/lab5/lab5-2.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct studentNode {
+    char name[20] ;
+    int age ;
+    char sex ;
+    float gpa ;
+    struct studentNode *next ;
+} ;
+
+void ShowAll(struct studentNode **walk) ;
+
+void GoNext(struct studentNode ***now) {
+    if (**now != NULL) {
+        *now = &((**now)->next) ;
+    }
+}
+
+void InsNode(struct studentNode **now, char n[], int a, char s, float g) {
+    struct studentNode *newNode = (struct studentNode*)malloc(sizeof(struct studentNode)) ;
+    strcpy(newNode->name, n) ;
+    newNode->age = a ;
+    newNode->sex = s ;
+    newNode->gpa = g ;
+
+    newNode->next = *now ;
+    *now = newNode ;       
+}
+
+void DelNode(struct studentNode **now) {
+    if (*now == NULL) return ;
+    struct studentNode *temp = *now ;
+    *now = temp->next ; 
+    free(temp) ;
+}
+
+int main() {
+    struct studentNode *start, **now ;
+    start = NULL ; 
+    now = &start ; 
+
+    InsNode(now, "one", 6, 'M', 3.11) ; ShowAll(&start) ;
+    InsNode(now, "two", 8, 'F', 3.22) ; ShowAll(&start) ;
+    GoNext(&now) ;
+    InsNode(now, "three", 10, 'M', 3.33) ; ShowAll(&start) ;
+    InsNode(now, "four", 12, 'F', 3.44) ; ShowAll(&start) ;
+    GoNext(&now) ;
+    DelNode(now) ; ShowAll(&start) ;
+
+    return 0 ;
+}
+
+void ShowAll(struct studentNode **walk) {
+    struct studentNode *temp = *walk ;
+    while (temp != NULL) {
+        printf("%s ", temp->name) ;
+        temp = temp->next ;
+    }
+    printf("\n") ;
+}
